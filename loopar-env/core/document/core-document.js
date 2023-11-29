5ac1d2ea963f6fa92445ca5edfe02547e20a7193
Alfredo <alfredrz2012@gmail.com>
2023-11-29 00:35:06 -0500

    Update core

--- loopar-env/core/document/core-document.js
+++ loopar-env/core/document/core-document.js
@@ -222,7 +222,7 @@ export default class CoreDocument {
             }
          }
 
-         await updateChild();
+         if(!loopar.installer) await updateChild();
 
          /*const updateConnections = async () => {
             if(["Document History"].includes(this.__DOCTYPE__.name)) return;
