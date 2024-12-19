85f7a3abdee0c9275e503f27117b59488e023074
aramirezarg <alfredrz2012@gmail.com>
2024-12-19 17:27:42 -0500

    Update build scripts and clean up configuration files

--- bin/build-clean.js
+++ bin/build-clean.js
@@ -3,7 +3,7 @@ const framework = process.cwd();
 
 async function cleanDistFolder() {
   try {
-    await rm('dist', { recursive: true, force: true });
+    await rm('../dist', { recursive: true, force: true });
     console.log('The "dist" folder has been successfully removed.');
   } catch (error) {
     console.error('An error occurred while removing the "dist" folder:', error);
--- bin/installer-clean.js
+++ bin/installer-clean.js
@@ -3,9 +3,9 @@ const framework = process.cwd();
 
 async function cleanDistFolder() {
   try {
-    await rm('package-lock.json', { recursive: true, force: true });
-    await rm('yarn.lock', { recursive: true, force: true });
-    await rm('pnpm-lock.yaml', { recursive: true, force: true });
+    await rm('../package-lock.json', { recursive: true, force: true });
+    await rm('../yarn.lock', { recursive: true, force: true });
+    await rm('../pnpm-lock.yaml', { recursive: true, force: true });
     
   } catch (error) {
     console.error('An error occurred while installer clean:', error);
