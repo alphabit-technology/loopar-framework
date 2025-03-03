c9ef6c9889c0f9038a6535506ca40321a74d9867
Alfredo <alfredrz2012@gmail.com>
2025-03-02 22:23:21 -0500

    feat: update theme colors and improve theme selector component

--- packages/loopar/src/components/theme.jsx
+++ packages/loopar/src/components/theme.jsx
@@ -1,21 +1,22 @@
 import { Button } from "@cn/components/ui/button";
-import {CircleIcon, CheckCircle} from "lucide-react";
-import { themes, showColors } from "@global/themes";
+import {CheckCircle} from "lucide-react";
+import { themes } from "@global/themes";
 import { titleize } from "inflection";
 import BaseInput from "@base-input";
 
-const Selector = ({ color, text, onSelect, active }) => {
+const Selector = ({ color, twcolor, text, onSelect, active }) => {
   const handleClick = (e) => {
     e.preventDefault();
     onSelect({ target: { value: color } });
   }
+  
   return (
     <Button
       variant="ghost"
-      className="w-full inline-flex items-center justify-start gap-2"
+      className={`w-full inline-flex items-center justify-start gap-2 ${active === color ? 'bg-primary/50 text-white' : 'text-primary'}`}
       onClick={handleClick}
     >
-      <span className={`${showColors[color]} w-5 h-5 rounded-full`}>
+      <span className={`${twcolor} w-5 h-5 rounded-full`}>
         {active === color && <CheckCircle className="w-5 h-5 text-white" />}
       </span>
       {text}
@@ -26,13 +27,14 @@ const Selector = ({ color, text, onSelect, active }) => {
 export function ThemeSelector({ onSelect, active }) {
   return (
     <div className="grid grid-flow-col grid-rows-4 gap-4 border border-border rounded-md p-4">
-      {Object.keys(themes).map((color) => {
+      {themes.map(theme => {
         return (
           <Selector
-            color={color}
-            text={titleize(color)}
+            color={theme.name}
+            twcolor={theme.twcolor}
+            text={titleize(theme.name)}
             onSelect={onSelect}
-            key={color}
+            key={theme.color}
             active={active}
           />
         );
